2970bb0bdfff5d050f579fa73b30926f14fe4cf5
Temitoyosi Osibemekun <toyosi.osibemekun@ietech.com.ng>
2022-10-22 22:05:14 +0100

    created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Header({text}) {
+  return (
+    <header>
+        <div className="container">
+            <h1>{text}</h1>
+        </div>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    text: "My Todo App",
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+}
+
+export default Header
